61fe58593dcc115077055323a5779434000c69f8
stwahyuu <septiantw27@gmail.com>
2021-06-02 20:23:15 +0700

    Membuat Class Main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Septian";
+        String namaTengah = "Tri";
+        String namaBelakang = "Wahyudin";
+        int usia = 19;
+        int TargetTahunKuliah = 4;
+        double ipk = 3.900;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
